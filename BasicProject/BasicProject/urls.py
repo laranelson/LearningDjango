54823c7f80b30434a6a31560fa5f3e4f346fe7fd
Nelson <nelsonlarajr@gmail.com>
2022-06-27 11:38:22 -0300

    Etapa 2-2: Executar o aplicativo a partir do projeto do Django

--- BasicProject/BasicProject/urls.py
+++ BasicProject/BasicProject/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import include, url
 import HelloDjangoApp.views
-
+import Soma.views
 # Django processes URL patterns in the order they appear in the array
 urlpatterns = [
     url(r'^$', HelloDjangoApp.views.index, name='index'),
     url(r'^home$', HelloDjangoApp.views.index, name='home'),
+    url(r'^soma$', Soma.views.soma, name='soma'),
+
 ]
